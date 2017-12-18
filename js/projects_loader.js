44be3ad7cfa337eb6ece8784fdee3687509f5135
michael-valdron <michael.valdron@gmail.com>
2017-12-18 02:00:19 -0500

    projects_loader.js added. AJAX and JSON code added to projects_loader.js. All active pages now have dynamic Top Projects driven by projects.json.

---
+++ js/projects_loader.js
@@ -0,0 +1,44 @@
+/**
+ * @author Michael Valdron
+ * Creation Date: Dec 17, 2017
+ * References: p5.js - https://p5js.org/
+ *
+ * Data Structure Tree
+ * ===================
+ * projects
+ * |_ project_name
+ * |_ catagory
+ * |_ project_links
+ * | |_ label
+ * | |_ url
+ * |_ prim_link
+ * |_ top_project
+ */
+
+var PATH = "json/projects.json";
+
+function loadData() {
+    var req = new XMLHttpRequest();
+    req.open('GET', PATH);
+    return req;
+}
+
+function projectsHeader() {
+    json_req = loadData();
+    json_req.onreadystatechange = function() {
+        var data = JSON.parse(json_req.responseText);
+        var html = "<li><strong>Top Projects</strong></li>";
+
+        for (var i = 0; i < data.projects.length; i++) {
+            if (data.projects[i].top_project) {
+                html += "<li><a href=\"" + data.projects[i].project_links[data.projects[i].prim_link].url + "\" target=\"_blank\">" + data.projects[i].project_name + "</a></li>";
+            }
+        }
+        html += "<li class=\"divider\"></li>" +
+                "<li>" +
+                "<a href=\"projects.html\">Show More Projects</a>" +
+                "</li>";
+        document.getElementById('projects-h').innerHTML = html;
+    }
+    json_req.send();
+}
