89b791d1bc61da2642b9e5d6ba55d4f9ac70864f
Michael Valdron <michael.valdron@gmail.com>
2018-06-01 05:21:08 -0400

    progress.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  
-  
+
+
   :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.9.0"]
@@ -13,7 +13,8 @@
                  [org.clojure/core.async  "0.4.474"]
                  [hipo "0.5.2"]
                  [domina "1.0.3"]
-                 [reagent "0.8.1"]]
+                 [reagent "0.8.1"]
+                 [cljs-ajax "0.7.3"]]
 
   :plugins [[lein-figwheel "0.5.16"]
             [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
