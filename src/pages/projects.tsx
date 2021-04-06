df80d94dbdf056eff0f92054d69952d991e6b108
Michael Valdron <michael.valdron@gmail.com>
2021-04-06 07:02:34 -0400

    init source for restful get project list.

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,11 +1,37 @@
-import React from "react"
+import React, { useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
 import Layout from "../components/layout";
 
-export default function Projects() {
-    /* TODO */
+type State = {loading: boolean, projects: Object[] | null};
+const initState: State = {
+    loading: false,
+    projects: null
+};
+const restInfo = {
+    protocol: 'http',
+    host: 'localhost',
+    port: 3000
+};
+
+export default function Projects() {  
+    const [appState, setAppState] = useState(initState);
+
+    useEffect(() => {
+        setAppState({ ...appState, loading: true });
+
+        axios.defaults.baseURL = `${restInfo.protocol}://${restInfo.host}${(restInfo.port) ? `:${restInfo.port}` : ''}`;
+        axios.get('/projects').then((res: AxiosResponse<string>) => {
+            const projects: Object[] = JSON.parse(res.data);
+
+            setAppState({ loading: false, projects: projects })
+        }).catch((err) => {
+            console.error(`An error occurred:\n${err}`);
+        });
+    }, [setAppState]);
+    
     return (
         <Layout pageTitle="Projects">
-            <div></div>
+            <div>{(appState.loading || appState.projects === null) ? "Loading..." : JSON.stringify(appState.projects)}</div>
         </Layout>
     );
 }
