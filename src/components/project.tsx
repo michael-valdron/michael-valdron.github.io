74839f51421345003011d9be44cdaa0522a33ed5
Michael Valdron <michael.valdron@gmail.com>
2021-04-10 03:00:22 -0400

    adjusted project component size.

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -16,11 +16,13 @@ export default class Project extends React.Component<ProjectProps, ProjectState,
         const completionDate = (this.props.completionDate) ? new Date(this.props.completionDate) : undefined;
 
         return (
-            <div className="uk-container uk-padding uk-margin-top uk-margin-bottom uk-card uk-card-default uk-card-hover uk-border-rounded uk-width-1-2@s">
+            <div className="uk-padding uk-margin-top uk-margin-bottom uk-card uk-card-default uk-card-hover uk-border-rounded uk-width-2-2@s">
                 <LinkLayout links={this.props.links}>
-                    <h1 className="uk-heading-small">{this.props.name}</h1>
-                    <h4>{(completionDate) ? `${new Intl.DateTimeFormat('en-US', { month: 'long' }).format(completionDate)} ${completionDate.getFullYear()}` : ""}</h4>
-                    <p>{this.props.description}</p>
+                    <div className="uk-margin-left">
+                        <h1 className="uk-heading-small">{this.props.name}</h1>
+                        <h4>{(completionDate) ? `${new Intl.DateTimeFormat('en-US', { month: 'long' }).format(completionDate)} ${completionDate.getFullYear()}` : ""}</h4>
+                        <p>{this.props.description}</p>
+                    </div>
                 </LinkLayout>
             </div>
         );
