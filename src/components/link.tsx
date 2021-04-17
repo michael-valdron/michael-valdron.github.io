d3ceae8dbbdb5770388646c64aeb3b639cca9f4d
Michael Valdron <michael.valdron@gmail.com>
2021-04-17 02:32:00 -0400

    link module readded for HeaderLink component.

---
+++ src/components/link.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type HeaderLinkProps = {label: string, link?: string}
+
+export function HeaderLink({ label, link }: HeaderLinkProps): JSX.Element {
+    const header = <h1 className="uk-heading-small">{label}</h1>;
+    if (link) {
+        return (
+            <a className="uk-link-toggle" href={link} target="__blank">
+                {header}
+            </a>
+        );
+    }
+    else {
+        return (
+            <div>
+                {header}
+            </div>
+        );
+    }
+}
