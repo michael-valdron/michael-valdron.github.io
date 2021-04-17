de143ea1d92010825d7b839f41945019d3eb906d
Michael Valdron <michael.valdron@gmail.com>
2021-04-17 03:03:58 -0400

    added missing semi colon.

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-type HeaderLinkProps = {label: string, link?: string}
+type HeaderLinkProps = {label: string, link?: string};
 
 export function HeaderLink({ label, link }: HeaderLinkProps): JSX.Element {
     const header = <h1 className="uk-heading-small">{label}</h1>;
