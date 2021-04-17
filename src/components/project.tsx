83c3f107597ac3cccfc091b43a0b08b92c1da8a2
Michael Valdron <michael.valdron@gmail.com>
2021-04-17 02:32:39 -0400

    HeaderLink component added to Project component.

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ProjectModel } from '../utils/types';
 import Keyword from './keyword';
+import { HeaderLink } from './link';
 
 type ProjectProps = {
     key: string
@@ -20,14 +21,11 @@ export default class Project extends React.Component<ProjectProps, ProjectState,
             <div className="uk-padding uk-margin-top uk-margin-bottom uk-card uk-card-default uk-card-hover uk-border-rounded uk-width-2-2@s">
                 <div className="uk-margin-left">
                     <div className="uk-child-width-1-2" uk-grid="true">
-                        <div>
-                            <h1 className="uk-heading-small">{this.props.name}</h1>
-                        </div>
+                        <HeaderLink label={this.props.name} link={this.props.links?.primary} />
                         <div className="uk-text-right">
                             <h4>{(completionDate) ? `${new Intl.DateTimeFormat('en-US', { month: 'long' }).format(completionDate)} ${completionDate.getFullYear()}` : ""}</h4>
                             {(this.props.keywords) ? <p>Keywords: {keywords}</p> : ""}
                         </div>
-                        {(this.props.links) ? <div><a href={this.props.links.primary} target="__blank">Preview</a></div> : ""}
                     </div>
                     <p>{this.props.description}</p>
                 </div>
