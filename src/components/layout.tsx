da3b18acb07e7e68c4741712d75419adff82e683
Michael Valdron <michael.valdron@gmail.com>
2021-06-15 21:44:21 -0400

    site layout is now responsive for mobile view.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import { Helmet } from "react-helmet"
 import { Link, useStaticQuery, graphql } from "gatsby";
 import { useIcons } from "../utils/icons";
 import "uikit/dist/css/uikit.min.css";
 import "uikit";
+
+const footerStyle: CSSProperties = {
+    paddingTop: "25px",
+    paddingBottom: "25px",
+    boxShadow: "0px -4px 3px rgba(50, 50, 50, 0.1)"
+};
 
 const active = (currentTitle: string, selectedTitle: string): string => 
     (currentTitle === selectedTitle) ? "uk-active" : "";
@@ -22,6 +28,7 @@ export default function Layout({ pageTitle, children }: LayoutProps): JSX.Elemen
             }
         `
     );
+
     useIcons();
 
     return (
@@ -30,22 +37,31 @@ export default function Layout({ pageTitle, children }: LayoutProps): JSX.Elemen
                 <meta charSet="utf-8" />
                 <title>{data.site.siteMetadata.title} - {pageTitle}</title>
             </Helmet>
-            <div className="uk-position-relative">
-                <nav className="uk-navbar-container" uk-navbar="true">
-                    <div className="uk-navbar-center">
-                    <ul className="uk-navbar-nav uk-navbar-center">
+            <nav className="uk-navbar uk-navbar-container" style={{height: "85px"}} uk-sticky="true" uk-navbar="true">
+                <a className="uk-navbar-toggle uk-hidden@s" href="#" uk-toggle="target: #mobile-nav">
+                    <span uk-navbar-toggle-icon="true"></span> <span className="uk-margin-small-left">Menu</span>
+                </a>
+                <div className="uk-navbar-center uk-visible@s">
+                    <ul className="uk-navbar-nav">
                         <li className={active(pageTitle, "Home")}><Link to="/">Home</Link></li>
                         <li className={active(pageTitle, "Projects")}><Link to="/projects">Projects</Link></li>
                         <li className={active(pageTitle, "Publications")}><Link to="/publications">Publications</Link></li>
                         <li className={active(pageTitle, "Interests")}><Link to="/interests">Interests</Link></li>
                     </ul>
-                    </div>
-                </nav>
+                </div>
+            </nav>
+            <div id="mobile-nav" uk-offcanvas="true">
+                <div className="uk-offcanvas-bar">
+                    <ul className="uk-nav uk-nav-default">
+                        <li className={[active(pageTitle, "Home"), "uk-text-large"].join(" ")}><Link to="/">Home</Link></li>
+                        <li className={[active(pageTitle, "Projects"), "uk-text-large"].join(" ")}><Link to="/projects">Projects</Link></li>
+                        <li className={[active(pageTitle, "Publications"), "uk-text-large"].join(" ")}><Link to="/publications">Publications</Link></li>
+                        <li className={[active(pageTitle, "Interests"), "uk-text-large"].join(" ")}><Link to="/interests">Interests</Link></li>
+                    </ul>
+                </div>
             </div>
-            <div className="uk-position-relative">
-                {children}
-            </div>
-            <div className="uk-position-relative uk-text-center uk-margin-top uk-margin-bottom">
+            {children}
+            <div className="uk-text-center" style={footerStyle}>
                 <div>
                     <a href="https://www.linkedin.com/in/michael-valdron-3b8614a5/" rel="noreferrer" target="_blank" className="uk-icon-button uk-button-secondary" uk-icon="linkedin">{""}</a>
                     &nbsp;
