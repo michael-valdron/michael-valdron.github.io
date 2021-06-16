b08b4b52a63f3331fe3d09dac0ef3a0f235a64f3
Michael Valdron <michael.valdron@gmail.com>
2021-06-15 21:43:35 -0400

    fixed up photo cover for pages.

--- src/components/cover.tsx
+++ src/components/cover.tsx
@@ -1,17 +1,11 @@
 import React from "react";
 
-const coverImage = (img: string) => ({
-    background: `linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4) ), url('${img}')`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: '100% auto'
-});
-
 type CoverProps = {title: string, img: string};
 
 export default function Cover({ title, img }: CoverProps): JSX.Element {
     return (
-        <div className="uk-height-large uk-background-cover uk-light uk-flex" style={coverImage(img)}>
-            <h1 style={{ fontWeight: "bolder" }} className="uk-width-1-1@m uk-text-center uk-margin-auto uk-margin-auto-vertical">{title}</h1>
+        <div className="uk-background-blend-overlay uk-background-secondary uk-background-cover uk-height-large uk-light uk-flex" style={{backgroundImage: `url(${img})`}}>
+            <h1 style={{ fontWeight: "bolder" }} className="uk-text-center uk-margin-auto uk-margin-auto-vertical">{title}</h1>
         </div>
     );
 }
